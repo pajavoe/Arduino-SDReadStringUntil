a772b47ca7f3dd50e76f48c0487e6fe339b2d134
pajavoe <52090518+pajavoe@users.noreply.github.com>
2020-11-18 06:23:05 +0100

    Update getLineAt.ino

--- getLineAt.ino
+++ getLineAt.ino
@@ -17,6 +17,24 @@ String getLineAt(unsigned int lineNumber, File myFile) {
   return output;
 }
 
+String getSetlineAt(unsigned int lineNumber, File myFile, int lengthLine) {
+  //not Tested!
+  //use this method if line length is known static
+  String output;
+  char cr;
+  for (int Line = 0; Line < (lineNumber * lengthLine); Line += lengthLine)  {
+    myFile.seek(Line);// jumps through each line until wanted
+  }
+  while (true) { //reads the line that is given
+    cr = myFile.read();
+    if (cr == '\n') {
+      break;
+    }
+    output += cr; //adds to output
+  }
+  return output;
+}
+
 /* Call like following:
 File myFile;
   myFile = SD.open("ErrorC.txt"); //open file in read mode
