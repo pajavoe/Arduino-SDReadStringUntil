ac912f8f93e016858462315664ba2af83df5ac81
pajavoe <52090518+pajavoe@users.noreply.github.com>
2020-11-18 06:29:31 +0100

    Update GetLineFromEnd.ino

--- GetLineFromEnd.ino
+++ GetLineFromEnd.ino
@@ -2,6 +2,7 @@ String getLineFromEnd(unsigned int linefromEnd, File myFile, unsigned int Reflin
   //ex. to get last line from myFile set linefromEnd = 0
   //unsigned int linefromEnd - (lastline - linefromEnd), File myFile - file to get line from,
   // unsigned int ReflineNumber - line where length of myFile is safed, File RefFile - length of myFile is safed
+  //see library for explanation for referece file
   int myFileSize =  getLineLength(ReflineNumber, RefFile); //looks up reference file, where length of myFile is saved
   String output = getLineAt(myFileSize - linefromEnd, myFile); //gets line from myFile
   return output;
