1ccf57b344000c8c0d154574ae08a93ba8858588
pajavoe <52090518+pajavoe@users.noreply.github.com>
2020-11-18 04:39:57 +0100

    Create ErrorDocumentation.ino

---
+++ ErrorDocumentation.ino
@@ -0,0 +1,12 @@
+void Error(String errorMessage, bool SDWrite) {
+  //String errorMessage - message to be send to Serial device and written on SD Card,
+  //optional bool SDWrite - write on SD
+  //call to safe/display Errors
+  int currentTime = (millis() / 1000);
+  if (SDCardDocumentation && SDWrite) {
+    writeOnSD(ErrorFile,  (String(currentTime) + "-" + errorMessage));//SD documentation
+  }
+  String SerialErrorMessage = ("S0x¿SE"  + String(currentTime) + "-" + errorMessage); //error Syntax for Mastermodule
+  sendSerial(SerialErrorMessage);//Serial documentation
+  ErrorBlinkOn = true; // turn on error blink-LED
+}
